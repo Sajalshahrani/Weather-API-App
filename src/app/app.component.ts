10f55e2a01a0c6fb22d64f894f66081cab6bc737
SajaAlshahrani <113879137+SajaAlshahrani@users.noreply.github.com>
2024-10-09 21:55:02 +0300

    title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,13 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private titleService: Title) {
+    this.setTitle('Weather-App');  
+  }
 
+  setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
 
 
